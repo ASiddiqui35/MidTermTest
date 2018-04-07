4f1b826e2a73004519f97a0b8ebadffec582ab6d
mrahman <rahmanww@gmail.com>
2018-04-07 12:01:41 -0400

    "Unit testing with TestNG"

---
+++ src/unit/testing/ApplyCalculator.java
@@ -0,0 +1,15 @@
+package unit.testing;
+
+import org.testng.Assert;
+
+public class ApplyCalculator {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        Assert.assertEquals(cal.addition(10,15),25);
+        Assert.assertEquals(cal.subtraction(20,10),10);
+        Assert.assertEquals(cal.multiplication(10,10),100);
+        cal.setName("Boby");
+        Assert.assertEquals("Boby", cal.getName(),"We could not find: "+cal.getName()+" cause he missed java class");
+
+    }
+}
